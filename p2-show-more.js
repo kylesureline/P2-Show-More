82f908d59c0e860e059751b09bbab20c050b585f
Kyle Scheuerlein <kscheuerlein@icloud.com>
2016-06-28 15:53:27 -0700

    Update p2-show-more.js

--- p2-show-more.js
+++ p2-show-more.js
@@ -4,7 +4,7 @@ $.fn.exists = function() {
 }
 function createExcerpts() {
     // do nothing on single post or page views
-    if($('body').hasClass('blog')) {
+    if(!$('body').hasClass('single') || !$('body').hasClass('page')) {
         // duplicate each post, adding appropriate classes
         $('.postcontent').each(function() {
             if($(this).hasClass('excerptCreated')) {

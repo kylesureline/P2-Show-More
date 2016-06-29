77c704fb2fc9210260287ba9ddad9c5df9a2604e
Kyle Scheuerlein <kscheuerlein@icloud.com>
2016-06-29 14:44:38 -0700

    Update show-more-p2.php

--- show-more-p2.php
+++ show-more-p2.php
@@ -3,7 +3,7 @@
 Plugin Name: Show More for P2
 Plugin URI: 
 Description: Automatically minimizes posts and adds a more link to display them inline.
-Version: 1.0
+Version: 1.1
 Author: Kyle Scheuerlein
 Author URI: http://www.kylescheuerlein.com
 License: GPL2
